15db619cf49eef7b6c86c924c4ed17a9aa171f57
Andy Kreling <krelinga@gmail.com>
2025-12-24 23:53:57 +0000

    formatting.

--- internal/jobs.go
+++ internal/jobs.go
@@ -5,8 +5,8 @@ import "github.com/google/uuid"
 // InfoJobArgs contains the arguments for an info job.
 // This is used as the River job args payload.
 type InfoJobArgs struct {
-	UUID            uuid.UUID `json:"uuid"`
-	Path      string    `json:"path"`
+	UUID uuid.UUID `json:"uuid"`
+	Path string    `json:"path"`
 }
 
 // Kind returns the job kind identifier for River.
